12cbbaf8fb6ae3d71573e5958535c818e255abcd
22011345-NurFarhah <22011345@myrp.edu.sg>
2023-08-10 09:47:25 +0800

    St

--- C206_CaseStudy/src/BikerCommunityPortal.java
+++ C206_CaseStudy/src/BikerCommunityPortal.java
@@ -1,4 +1,7 @@
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class BikerCommunityPortal {
 
@@ -9,31 +12,36 @@ public class BikerCommunityPortal {
 
 		int option = 0;
 
-
-
 		while (option != 4) {
-
-
 
 			BikerCommunityPortal.menu();
 			option = Helper.readInt("Enter an option > ");
 
-
-
 			if (option == 1) {
-				
-			}
-			else if (option == 2) {
 
-			}
-			else if (option == 3) {
-				
-			}
-			else if (option == 4) {
+			} else if (option == 2) {
+
+			} else if (option == 3) {
+				int optionEvents = 0;
+				BikerCommunityPortal.EventsMenu();
+				while (optionEvents != 4) {
+					optionEvents = Helper.readInt("Enter event option > ");
+					if (optionEvents == 1) {
+
+					} else if (optionEvents == 2) {
+
+					} else if (optionEvents == 3) {
+
+					} else if (optionEvents == 4) {
+						System.out.println("Bye!");
+					}
+				}
+			} else if (option == 4) {
 				System.out.println("Bye!");
 			}
 		}
 	}
+
 	public static void menu() {
 		BikerCommunityPortal.setHeader("Biker Community Portal");
 		System.out.println("1. Users");
@@ -42,6 +50,16 @@ public class BikerCommunityPortal {
 		System.out.println("4. Quit");
 		Helper.line(80, "-");
 	}
+
+	public static void EventsMenu() {
+		BikerCommunityPortal.setHeader("Events");
+		System.out.println("1. View Events");
+		System.out.println("2. Add Events");
+		System.out.println("3. Delete Events");
+		System.out.println("4. Quit");
+		Helper.line(80, "-");
+	}
+
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,33 @@
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class C206_CaseStudyTest {
+public class C206_CaseStudyTest{
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	@Test
+	public void testAddEvents() {
+
 	}
 
 	@Test
 	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+		// fail("Not yet implemented");
+		assertTrue("C206_CaseStudy_SampleTest ", true);
+	}
+
+	@After
+	public void tearDown() throws Exception {
 	}
 
 }
