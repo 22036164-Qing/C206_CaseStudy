1d24533e129c1398dd11b57ad64904e25bdb028a
22011345-NurFarhah <22011345@myrp.edu.sg>
2023-08-17 13:05:40 +0800

    updated Bike Class

---
+++ C206_CaseStudy/src/codes/Bike.java
@@ -0,0 +1,96 @@
+package codes;
+public class Bike {
+
+		String name;
+
+		String brand;
+
+		String platenumber;
+
+		int mileage;
+
+		int owners;
+
+	 
+
+		public Bike(String name, String brand, String platenumber, int mileage, int owners) {
+
+			
+
+			this.name = name;
+
+	 
+
+			this.brand = brand;
+
+			this.platenumber = platenumber;
+
+			this.mileage = mileage;
+
+			this.owners = owners;
+
+		}
+
+	 
+
+	 
+
+	 
+
+		public String getName() {
+
+			return name;
+
+		}
+
+	 
+
+	 
+
+		public String getBrand() {
+
+			return brand;
+
+		}
+
+	 
+
+	 
+
+	 
+
+		public String getPlatenumber() {
+
+			return platenumber;
+
+		}
+
+	 
+
+	 
+
+	 
+
+		public int getMileage() {
+
+			return mileage;
+
+		}
+
+	 
+
+	 
+
+	 
+
+		public int getOwners() {
+
+			return owners;
+
+		}
+
+	 
+
+	 
+
+	}
